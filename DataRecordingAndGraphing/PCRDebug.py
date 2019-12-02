27e6b7f0c6438a508da026f522c33113749a2a04
phooobs <37228509+phooobs@users.noreply.github.com>
2019-12-02 16:20:52 -0700

    Added arduino comunication

--- DataRecordingAndGraphing/PCRDebug.py
+++ DataRecordingAndGraphing/PCRDebug.py
@@ -46,6 +46,7 @@ class serialGraphApp(QtGui.QMainWindow):
         self.TPTemp = [] # target peltier temp over time
         self.pSignal = [] # curent signal being sent to curent drivers
         self.recordTime = [] # what time the points where recorded at
+        self.targetPeltierTemp = 27
         for i in range(2000):
            self.CPTemp.append(0)
            self.TPTemp.append(0)
@@ -66,6 +67,7 @@ class serialGraphApp(QtGui.QMainWindow):
         self.comBtn.move(0,30)
         self.connected = False
         self.collect = False
+        self.isCycle = False
 
         self.comText = QtGui.QLabel("COM", self)
         self.comText.move(13, 1)
@@ -79,27 +81,51 @@ class serialGraphApp(QtGui.QMainWindow):
         self.runBtn.resize(100, 30)
         self.runBtn.move(0, 60)
 
+        self.cycleBtn = QtGui.QPushButton("Cycle", self)
+        self.cycleBtn.clicked.connect(self.cycleOnOff)
+        self.cycleBtn.resize(100, 30)
+        self.cycleBtn.move(0, 90)
+        
+        
+        self.setBtn = QtGui.QPushButton("Set", self)
+        self.setBtn.clicked.connect(self.setTemp)
+        self.setBtn.resize(50, 30)
+        self.setBtn.move(0, 120)
+        
+        self.setTextBox = QtGui.QLineEdit(self)
+        self.setTextBox.resize(50, 30)
+        self.setTextBox.move(50, 120)
+        
+        self.sendBtn = QtGui.QPushButton("Send", self)
+        self.sendBtn.clicked.connect(self.sendCommand)
+        self.sendBtn.resize(50, 30)
+        self.sendBtn.move(0, 150)
+        
+        self.sendTextBox = QtGui.QLineEdit(self)
+        self.sendTextBox.resize(50, 30)
+        self.sendTextBox.move(50, 150)
+        
         self.zoom = QtGui.QSlider(QtCore.Qt.Horizontal, self)
         self.zoom.setRange(50, 2000)
         self.zoom.resize(100, 30)
-        self.zoom.move(0, 90)
+        self.zoom.move(0, 180)
         self.zoom.setValue(1000)
         
         self.graphBtn = QtGui.QPushButton("Graph", self)
         self.graphBtn.clicked.connect(self.plotData)
         self.graphBtn.resize(100, 30)
-        self.graphBtn.move(0, 120)
+        self.graphBtn.move(0, 210)
 
         self.graphText = QtGui.QLabel("Start     Length", self)
-        self.graphText.move(4, 151)
+        self.graphText.move(4, 241)
 
         self.startTextBox = QtGui.QLineEdit(self)
         self.startTextBox.resize(50, 30)
-        self.startTextBox.move(0, 180)
+        self.startTextBox.move(0, 270)
 
         self.lenTextBox = QtGui.QLineEdit(self)
         self.lenTextBox.resize(50, 30)
-        self.lenTextBox.move(50, 180)
+        self.lenTextBox.move(50, 270)
 
         self.plotPT = pg.PlotWidget(self)
         self.plotPT.hideAxis("bottom")
@@ -123,16 +149,18 @@ class serialGraphApp(QtGui.QMainWindow):
 
     def comConnect(self):
         if self.connected and not self.collect:
-            #self.port.close()
+            self.port.close()
             self.connected = False
             self.comBtn.setText("Connect")
             self.comTextBox.setReadOnly(False)
         elif not self.collect:
             try:
-                #self.port = serial.Serial('COM' + str(self.comTextBox.text()), 115200, timeout=0.5) 
+                self.port = serial.Serial('COM' + str(self.comTextBox.text()), 9600, timeout=0.5) 
                 self.connected = True
                 self.comBtn.setText("Disconnect")
                 self.comTextBox.setReadOnly(True)
+                self.port.write("off\n".encode())
+                self.port.write(("pt" + str(self.targetPeltierTemp) + "\n").encode())
             except:
                 pass
 
@@ -140,34 +168,68 @@ class serialGraphApp(QtGui.QMainWindow):
     def startStop(self):
         if self.collect and self.connected:
             self.collect = False
+            self.isCycle = False
+            self.port.write("off\n".encode())
             self.runBtn.setText("Start")
+            self.cycleBtn.setText("Cycle")
         elif self.connected:
             self.collect = True
+            self.port.write("on\n".encode())
             self.runBtn.setText("Stop")
-                
-            #self.port.flushInput()
+            self.port.flushInput()
 
+    def setTemp(self):
+        self.targetPeltierTemp = float(self.setTextBox.text())
+        self.setTextBox.clear()
+        
+    def sendCommand(self):
+        if self.sendTextBox.text()[0:2] == "pt":
+            self.targetPeltierTemp = float(self.sendTextBox.text()[2:])
+        else:
+            self.port.write((self.sendTextBox.text() + "\n").encode())
+        self.sendTextBox.clear()
+    
+    def cycleOnOff(self):
+        if self.isCycle:
+            self.isCycle = False
+            self.cycleBtn.setText("Cycle")
+        elif self.collect:
+            self.isCycle = True
+            self.cycleBtn.setText("Stop")
             self.cycles.clear()
-            self.cycles.add(self.cycle(40, 5))
-            self.cycles.add(self.cycle(70, 5))
-            self.cycles.add(self.cycle(40, 5))
-            self.cycles.add(self.cycle(70, 5))
-            self.cycles.add(self.cycle(40, 5))
-            self.cycles.add(self.cycle(70, 5))
-            self.cycles.add(self.cycle(40, 5))
-            self.cycles.add(self.cycle(70, 5))
+            self.cycles.add(self.cycle(40, 20))
+            self.cycles.add(self.cycle(70, 20))
+            self.cycles.add(self.cycle(40, 20))
+            self.cycles.add(self.cycle(70, 20))
+            self.cycles.add(self.cycle(40, 20))
+            self.cycles.add(self.cycle(70, 20))
             self.cycles.add(self.cycle(55, 5))
 
     def update(self):
-        if self.collect:
-            #portBytes = self.port.readline()
-            #line = str(portBytes[0 : len(portBytes) - 2].decode("utf-8"))
-            #data = [float(num) for num in line.split()]
-            self.TPTemp.append(self.cycles.getTemp())
+        if self.connected:
+            data = []
+            try:
+                portBytes = self.port.readline()
+                line = str(portBytes[0 : len(portBytes) - 2].decode("utf-8"))
+                data = [float(num) for num in line.split()]
+            except:
+                pass
+            
+            if self.isCycle:
+                self.targetPeltierTemp = self.cycles.getTemp()
+                
+            if self.targetPeltierTemp != self.TPTemp[len(self.TPTemp) - 1]: # sends command for temp set
+                self.port.write(("pt" + str(self.targetPeltierTemp) + "\n").encode())
+            
+            self.TPTemp.append(self.targetPeltierTemp) 
             self.TPTemp.pop(0)
-            self.CPTemp.append(random.randint(20, 90))
+            if len(data) == 2:
+                self.CPTemp.append(data[0])
+                self.pSignal.append(data[1])
+            else:
+                self.CPTemp.append(self.CPTemp[len(self.CPTemp) - 1])
+                self.pSignal.append(self.pSignal[len(self.pSignal) - 1])
             self.CPTemp.pop(0)
-            self.pSignal.append(random.randint(-1024, 1024))
             self.pSignal.pop(0)
             self.recordTime.append(time.time())
             self.recordTime.pop(0)
@@ -177,7 +239,7 @@ class serialGraphApp(QtGui.QMainWindow):
         self.plotPT.plot(self.X, self.TPTemp, pen = self.pennTPTemp, clear = False)
         self.plotPS.plot(self.X, self.pSignal, pen = self.pennPSignal, clear = True)
         QtCore.QTimer.singleShot(1, self.update)
-
+        
     def plotData(self):
         readTemp = copy.deepcopy(self.CPTemp)
         targetTemp = copy.deepcopy(self.TPTemp)
@@ -215,9 +277,10 @@ class serialGraphApp(QtGui.QMainWindow):
         fig.tight_layout()
         fig.savefig("img.png", dpi = 500)
         plt.show()
-
+    
     def closeEvent(self, event):
         try:
+            self.port.write("off\n".encode())
             self.port.close()
         except:
             pass
