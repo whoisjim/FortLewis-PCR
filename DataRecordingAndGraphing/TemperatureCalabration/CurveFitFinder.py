f0cc54732cefbe676da2a681ef8a905f9a182c3f
James Ferguson <phooobs@gmail.com>
2020-02-06 21:28:15 -0700

    did calabration analisys data is definatly bad

--- DataRecordingAndGraphing/TemperatureCalabration/CurveFitFinder.py
+++ DataRecordingAndGraphing/TemperatureCalabration/CurveFitFinder.py
@@ -1,6 +1,13 @@
 import matplotlib.pyplot as plt
 import numpy
+import numpy as np
+from matplotlib.collections import LineCollection
+from matplotlib.colors import ListedColormap, BoundaryNorm
 from scipy.optimize import curve_fit
+
+startT = float(input("Please desired start time "))
+endT = float(input("Please desired length ")) + startT
+
 fileName = input("Please enter PCR file name ")
 PCRFile = open(str(fileName + ".txt"), 'r')
 fileName = input("Please enter Thermo file name ")
@@ -39,30 +46,67 @@ tFile.close()
 
 tData = [[float(num) for num in tData[0]], [float(num) for num in tData[1]], [float(num) for num in tData[2]], [float(num) for num in tData[3]], [float(num) for num in tData[4]],  [3600 * float(num[0:2]) + 60 * float(num[3:5]) + float(num[6:13]) - startTime for num in tData[5]]]
 
-# plot data and format
-fig = plt.figure(figsize=(20, 6))
-tempPlot = fig.add_subplot(211)
-func = fig.add_subplot(212)
-for i in range(5):
-    tempPlot.plot(tData[5], tData[i], c = "g", linewidth = 1.0)
+# clip data to specified bounds
+while times[0] < startT:
+    times.pop(0)
+    readTemp.pop(0)
+    targetTemp.pop(0)
+while times[len(times) - 1] > endT:
+    times.pop(len(times) - 1)
+    readTemp.pop(len(readTemp) - 1)
+    targetTemp.pop(len(targetTemp) - 1)
+times = [t - startT for t in times] # move times to start time
 
-tempPlot.plot(times, readTemp, c = "b", linewidth = 1.0)
-tempPlot.plot(times, targetTemp, c = "r", ls = "--", linewidth = 1.0)
-tempPlot.axis([0, elapsedTime, None, None])
-tempPlot.set_ylabel("Temperature (C)")
-tempPlot.tick_params( axis='x', which='both', bottom=False, top=False, labelbottom=False)
-tempPlot.legend(("T0", "T1", "T2", "T3", "T4", "Current Temperature", "Target Temperature"), loc="upper left")
-func.plot(readTemp, signal)
+# clip data to specified bounds
+while tData[5][0] < startT:
+    for i in range(6):
+        tData[i].pop(0)
+while tData[5][len(tData[5]) - 1] > endT:
+    for i in range(6):
+        tData[i].pop(len(tData[i]) - 1)
+tData[5] = [t - startT for t in tData[5]] # move times to start time
+
+# plot data and format
+fig, axs = plt.subplots(2, 1)
+for i in range(5):
+    axs[0].plot(tData[5], tData[i], c = "k", linewidth = 1.0)
+
+points = np.array([numpy.asarray(times), numpy.asarray(readTemp)]).T.reshape(-1, 1, 2)
+segments = np.concatenate([points[:-1], points[1:]], axis=1)
+lc = LineCollection(segments, cmap='jet')
+lc.set_array(numpy.asarray(times))
+lc.set_linewidth(1)
+line = axs[0].add_collection(lc)
+
+axs[0].plot(times, targetTemp, c = "r", ls = "--", linewidth = 1.0)
+axs[0].axis([0, endT - startT, None, None])
+
+def getClose(data, time, target):
+    for i in range(len(time)):
+        if target < time[i]:
+            return data[i]
+
+#closeData = [getClose(readTemp, times, target) for target in tData[5]] 
+
 def eq(x, m, b):
     return m * x + b
-popt, pcov = curve_fit(eq, readTemp[0:85919], tData[0])
-func.plot(readTemp[0:85919], tData[0]) # make actyally line up
+popt, pcov = curve_fit(eq, readTemp[0:len(tData[0])], tData[1])
+print("y =", popt[0], "* x +", popt[1])
+
+points = np.array([numpy.asarray(readTemp[0:len(tData[0])]), numpy.asarray(tData[1])]).T.reshape(-1, 1, 2)
+segments = np.concatenate([points[:-1], points[1:]], axis=1)
+lc = LineCollection(segments, cmap='jet')
+lc.set_array(numpy.asarray(tData[5]))
+lc.set_linewidth(1)
+line = axs[1].add_collection(lc)
+
+#func.plot(readTemp[0:85919], tData[1], c = "k", lineWidth = 1.0) # make actyally line up
+
 x = (0, 100)
 y = [eq(i, *popt) for i in x]
-func.plot(x, y)
+axs[1].plot(x, y, c = "k", lineWidth = 1.0)
 fig.tight_layout()
 fig.savefig(fileName + ".png", dpi = 500)
 plt.show()
-print("y =", popt[0], "* x +", popt[1])
-print(len(tData[5]), len(times))
+
 
