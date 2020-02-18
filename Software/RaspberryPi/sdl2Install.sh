98fd9385fedf796ab67c5ac374efc553a9ca5747
James Ferguson <37228509+phooobs@users.noreply.github.com>
2020-02-18 14:16:45 -0700

    Update sdl2Install.sh

--- Software/RaspberryPi/sdl2Install.sh
+++ Software/RaspberryPi/sdl2Install.sh
@@ -7,19 +7,23 @@ cd SDL
 make
 sudo make install
 cd ..
-wget http://www.libsdl.org/projects/SDL_image/release/SDL2_image-2.0.2.tar.gz
-wget http://www.libsdl.org/projects/SDL_ttf/release/SDL2_ttf-2.0.14.tar.gz
-tar zxvf SDL2_image-2.0.2.tar.gz
-tar zxvf SDL2_ttf-2.0.14.tar.gz
-cd SDL2_image-2.0.2 
-./autogen.sh 
-./configure --prefix=/usr
-make 
-sudo make install
-cd ..
-cd SDL2_ttf-2.0.14
-./autogen.sh
-./configure --prefix=/usr
-make
-sudo make install
-cd ..
+
+sudo apt-get install libsdl2-image-dev
+sudo apt-get install libsdl2-ttf-dev
+
+# wget http://www.libsdl.org/projects/SDL_image/release/SDL2_image-2.0.2.tar.gz
+# wget http://www.libsdl.org/projects/SDL_ttf/release/SDL2_ttf-2.0.14.tar.gz
+# tar zxvf SDL2_image-2.0.2.tar.gz
+# tar zxvf SDL2_ttf-2.0.14.tar.gz
+# cd SDL2_image-2.0.2 
+# ./autogen.sh 
+# ./configure --prefix=/usr
+# make 
+# sudo make install
+# cd ..
+# cd SDL2_ttf-2.0.14
+# ./autogen.sh
+# ./configure --prefix=/usr
+# make
+# sudo make install
+# cd ..
