911e3ccfd6eefc09bba48c43e7f63b504cfd2ac2
Ferguson <jwferguson@fortlewis.edu>
2020-02-20 23:27:48 +0000

    Started UI classes

--- Software/RaspberryPi/sdl2_hello.cpp
+++ Software/RaspberryPi/sdl2_hello.cpp
@@ -1,92 +1,41 @@
-#include <SDL2/SDL.h>
-#include <SDL2/SDL_image.h>
-#include <SDL2/SDL_ttf.h>
 #include <stdio.h>
+#include "ui.h"
 
-#define SCREEN_WIDTH 1920
-#define SCREEN_HEIGHT 1080
-
-SDL_Window* window = NULL;
-SDL_Renderer* renderer = NULL;
-SDL_Surface* screenSurface = NULL;
-SDL_Event event;
-
-SDL_Texture* textures[1];
 TTF_Font* font;
 bool run = true;
 
-void dispError() {
-	SDL_Surface* fonttemp = TTF_RenderText_Solid(font, SDL_GetError(), {255, 0, 0, 255});
-	SDL_Texture* fontTexture = SDL_CreateTextureFromSurface(renderer, fonttemp);
-	SDL_FreeSurface(fonttemp);
-	SDL_Rect* dest = new SDL_Rect;
-	dest -> x = 16;
-	dest -> y = 16;
-	dest -> h = 28;
-	dest -> w = 1888;
-	SDL_RenderCopy(renderer, fontTexture, NULL, dest);
-}
-
 int main(int argc, char* args[]) {
-	if (SDL_Init(SDL_INIT_EVERYTHING) < 0) {
-		fprintf(stderr, "could not initialize sdl2: %s\n", SDL_GetError());
-		return 1;
-	}
-	window = SDL_CreateWindow("FLC:PCR", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_FULLSCREEN);
-	if (window == NULL) {
-		fprintf(stderr, "could not create window: %s\n", SDL_GetError());
-		return 1;
-	}
-	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-	if (renderer == NULL) {
-		fprintf(stderr, "could not create renderer: %s\n", SDL_GetError());
-		return 1;
-	}
-	if (TTF_Init() == -1) {
-		fprintf(stderr, "could not initialize ttf: %s\n", SDL_GetError());
+
+	if (UI::init()) {
 		return 1;
 	}
 
 	font = TTF_OpenFont("fonts/consola.ttf", 28);
 	SDL_Surface* temp = TTF_RenderText_Solid(font, "Test", {255, 255, 255, 255});
-	SDL_Texture* fontTexture = SDL_CreateTextureFromSurface(renderer, temp);
+	SDL_Texture* fontTexture = SDL_CreateTextureFromSurface(UI::renderer, temp);
 	SDL_FreeSurface(temp);
-
-	temp = IMG_Load("img/rect.png");
-	textures[0] = SDL_CreateTextureFromSurface(renderer, temp);
-	SDL_FreeSurface(temp);
-	SDL_Rect * source = NULL;
-	
-	SDL_Rect * dest = new SDL_Rect;
-	dest -> x = 16;
-	dest -> y = 64;
-	dest -> h = 16;
-	dest -> w = 16;
 
 	while (run) {
-		while (SDL_PollEvent(&event) != 0) {
-			if (event.type == SDL_QUIT) {
+		while (SDL_PollEvent(&UI::event) != 0) {
+			if (UI::event.type == SDL_QUIT) {
 				run = false;
-			} else if (event.type == SDL_KEYDOWN) {
-				switch (event.key.keysym.sym) {
+			} else if (UI::event.type == SDL_KEYDOWN) {
+				switch (UI::event.key.keysym.sym) {
 					case SDLK_ESCAPE:
 						run = false;
 						break;
 				}
 			}
 		}
-		SDL_SetRenderDrawColor(renderer, 50, 50, 50, 255);
-		SDL_RenderClear(renderer);
+		SDL_SetRenderDrawColor(UI::renderer, 50, 50, 50, 255);
+		SDL_RenderClear(UI::renderer);
 		
-		SDL_RenderCopy(renderer, fontTexture, source, source);
-		SDL_RenderCopy(renderer, textures[0], source, dest);
+		SDL_RenderCopy(UI::renderer, fontTexture, NULL, NULL);
 		
-		dispError();
-		
-		SDL_RenderPresent(renderer);
+		SDL_RenderPresent(UI::renderer);
 	
 	}
-	SDL_DestroyWindow(window);
+	SDL_DestroyWindow(UI::window);
 	SDL_Quit();
 	return 0;
 }
---
+++ Software/RaspberryPi/ui.h
@@ -0,0 +1,90 @@
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_ttf.h>
+#include <string>
+#include <vector>
+
+#define SCREEN_WIDTH 1920
+#define SCREEN_HEIGHT 1080
+
+namespace UI {
+	SDL_Window* window = NULL;
+	SDL_Renderer* renderer = NULL;
+	SDL_Surface* screenSurface = NULL;
+	SDL_Event event;
+
+	std::vector<std::string> paths;
+	std::vector<SDL_Texture*> textures;
+
+	int init() {
+		if (SDL_Init(SDL_INIT_EVERYTHING) < 0) {
+			fprintf(stderr, "could not initialize sdl2: %s\n", SDL_GetError());
+			return 1;
+		}
+		window = SDL_CreateWindow("FLC:PCR", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_FULLSCREEN);
+		if (window == NULL) {
+			fprintf(stderr, "could not create window: %s\n", SDL_GetError());
+			return 1;
+		}
+		renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+		if (renderer == NULL) {
+			fprintf(stderr, "could not create renderer: %s\n", SDL_GetError());
+			return 1;
+		}
+		if (TTF_Init() == -1) {
+			fprintf(stderr, "could not initialize ttf: %s\n", SDL_GetError());
+			return 1;
+		}
+		return 0;
+	}
+
+	class Padding {
+		public:
+			int x_, y_, w_, h_;
+			
+			Padding (const char* path, int border, int w, int h, int x = 0, int y = 0) {
+				bool textureLoaded = false;
+				for (int i = 0; i < paths.size(); i++) {
+					if (paths[i] == path) {
+						textureLoaded = true;
+						textureID_ = i;
+						break;
+					}
+				}
+				if (!textureLoaded) {
+					textureID_ = paths.size();
+					paths.push_back(path);
+					SDL_Surface* tempSurface = IMG_Load(path);
+					textures.push_back(SDL_CreateTextureFromSurface(renderer , tempSurface));
+					SDL_FreeSurface(tempSurface);
+				}
+
+				x_ = x;
+				y_ = y;
+				w_ = w;
+				h_ = h;
+				border_ = border;
+			}
+			void render () {
+				SDL_Rect source;
+				SDL_Rect destination;
+
+				source.x = 0;
+				source.y = 0;
+				source.w = border_;
+				source.h = border_;
+				destination.x = x_;
+				destination.y = y_;
+				destination.w = border_;
+				destination.h = border_;
+				SDL_RenderCopy(renderer , textures[textureID_], &source, &destination);
+			}
+			std::string getPath() {
+				return paths[textureID_];
+			}
+		private:
+			int textureID_;
+			int border_;
+
+	};
+}
