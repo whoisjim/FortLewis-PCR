1283857646a69333cca8cc9896d447be8407232c
Raspberry Pi <jwferguson@fortlewis.edu>
2020-02-23 04:12:21 +0000

    renamed some files

---
+++ Software/RaspberryPi/compile.sh
@@ -0,0 +1,1 @@
+g++ -o flcpcr main.cpp $(SDL/sdl2-config --cflags --libs) -lSDL2_image -lSDL2_ttf -Wall
--- Software/RaspberryPi/testCompile.sh
+++
@@ -1,1 +0,0 @@
-g++ -o sdl2_hello sdl2_hello.cpp $(SDL/sdl2-config --cflags --libs) -lSDL2_image -lSDL2_ttf -Wall
