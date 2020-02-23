78a9a4e621c61ae57357e968b9a86c767526c2e5
Raspberry Pi <jwferguson@fortlewis.edu>
2020-02-23 04:03:55 +0000

    Created Cycle step UI

--- Software/RaspberryPi/sdl2_hello.cpp
+++ Software/RaspberryPi/sdl2_hello.cpp
@@ -1,55 +1,42 @@
 #include <stdio.h>
 #include "ui.h"
 
-TTF_Font* font;
 bool run = true;
 
 int main(int argc, char* args[]) {
 
-	if (UI::init()) {
-		return 1;
-	}
+  if (UI::init()) {
+    return 1;
+  }
+  { 
+    UI::CycleStep testStep0(10, 10);
+    UI::CycleStep testStep1(120, 10);
 
-	font = TTF_OpenFont("fonts/consola.ttf", 28);
-	SDL_Surface* temp = TTF_RenderText_Solid(font, "Test", {255, 255, 255, 255});
-	SDL_Texture* fontTexture = SDL_CreateTextureFromSurface(UI::renderer, temp);
-	SDL_FreeSurface(temp);
-	
-	UI::Padding testPadD("img/default_padding.png", 6, 120, 120, 0, 0);
-	UI::Padding testPadG("img/grey_padding.png", 6, 100, 100, 10, 10);
-	UI::Padding testPadK("img/dark_padding.png", 6, 80, 80, 20, 20);
-	UI::Padding testPadS("img/sharp_padding.png", 6, 60, 60, 30, 30);
-	UI::Padding testPadC("img/sharp_selection_padding.png", 6, 40, 40, 40, 40);
+    while (run) {
+      while (SDL_PollEvent(&UI::event) != 0) {
+        if (UI::event.type == SDL_QUIT) {
+          run = false;
+        } else if (UI::event.type == SDL_KEYDOWN) {
+          switch (UI::event.key.keysym.sym) {
+            case SDLK_ESCAPE:
+              run = false;
+              break;
+          }
+        }
+      }
+      SDL_SetRenderDrawColor(UI::renderer, 90, 105, 136, 255);
+      SDL_RenderClear(UI::renderer);
+      static float pos = 120;
+      pos += 0.1;
+      testStep1.setXY(pos, 10);
 
-
-
-	while (run) {
-		while (SDL_PollEvent(&UI::event) != 0) {
-			if (UI::event.type == SDL_QUIT) {
-				run = false;
-			} else if (UI::event.type == SDL_KEYDOWN) {
-				switch (UI::event.key.keysym.sym) {
-					case SDLK_ESCAPE:
-						run = false;
-						break;
-				}
-			}
-		}
-		SDL_SetRenderDrawColor(UI::renderer, 50, 50, 50, 255);
-		SDL_RenderClear(UI::renderer);
-		
-		SDL_RenderCopy(UI::renderer, fontTexture, NULL, NULL);
-
-		testPadD.render();
-		testPadG.render();
-		testPadK.render();
-		testPadS.render();
-		testPadC.render();
-		
-		SDL_RenderPresent(UI::renderer);
-	
-	}
-	SDL_DestroyWindow(UI::window);
-	SDL_Quit();
-	return 0;
+      testStep0.render();
+      testStep1.render();
+    
+      SDL_RenderPresent(UI::renderer);
+    } 
+  }
+  SDL_DestroyWindow(UI::window);
+  SDL_Quit();
+  return 0;
 }
--- Software/RaspberryPi/ui.h
+++ Software/RaspberryPi/ui.h
@@ -13,8 +13,11 @@ namespace UI {
   SDL_Surface* screenSurface = NULL;
   SDL_Event event;
 
-  std::vector<std::string> paths;
+  std::vector<std::string> texturePaths;
   std::vector<SDL_Texture*> textures;
+
+  std::vector<std::string> fontPaths;
+  std::vector<TTF_Font*> fonts;
 
   int init() {
     if (SDL_Init(SDL_INIT_EVERYTHING) < 0) {
@@ -39,32 +42,34 @@ namespace UI {
   }
 
   class Padding {
-    public:
-      int x_, y_, w_, h_;
-      
-      Padding (const char* path, int border, int w, int h, int x = 0, int y = 0) {
-        bool textureLoaded = false;
-        for (int i = 0; i < paths.size(); i++) {
-          if (paths[i] == path) {
-            textureLoaded = true;
-            textureID_ = i;
-            break;
-          }
-        }
-        if (!textureLoaded) {
-          textureID_ = paths.size();
-          paths.push_back(path);
-          SDL_Surface* tempSurface = IMG_Load(path);
-          textures.push_back(SDL_CreateTextureFromSurface(renderer , tempSurface));
-          SDL_FreeSurface(tempSurface);
-        }
-
+    public: 
+      Padding (const char* path, int border, int x, int y, int w, int h) {
+        setTexture(path);
         x_ = x;
         y_ = y;
         w_ = w;
         h_ = h;
         border_ = border;
       }
+
+      void setTexture (const char* path) {
+        bool textureLoaded = false;
+        for (unsigned int i = 0; i < texturePaths.size(); i++) {
+          if (texturePaths[i] == path) {
+            textureLoaded = true;
+            textureID_ = i;
+            break;
+          }
+        }
+        if (!textureLoaded) {
+          textureID_ = texturePaths.size();
+          texturePaths.push_back(path);
+          SDL_Surface* tempSurface = IMG_Load(path);
+          textures.push_back(SDL_CreateTextureFromSurface(renderer, tempSurface));
+          SDL_FreeSurface(tempSurface);
+        }
+      }
+
       void render () {
         SDL_Rect source;
         SDL_Rect destination;
@@ -91,7 +96,7 @@ namespace UI {
         destination.w = w_ - 2 * border_;
         destination.h = border_;
         SDL_RenderCopy(renderer, textures[textureID_], &source, &destination);
-	
+  
         source.x = imgW - border_;
         source.y = 0;
         source.w = border_;
@@ -101,7 +106,7 @@ namespace UI {
         destination.w = border_;
         destination.h = border_;
         SDL_RenderCopy(renderer, textures[textureID_], &source, &destination);
-       	
+  
         source.x = 0;
         source.y = border_;
         source.w = border_;
@@ -161,14 +166,194 @@ namespace UI {
         destination.w = border_;
         destination.h = border_;
         SDL_RenderCopy(renderer, textures[textureID_], &source, &destination);
-        
       }
+
+      void setXY (int x, int y) {
+        x_ = x;
+        y_ = y;
+      }
+
       std::string getPath() {
-        return paths[textureID_];
+        return texturePaths[textureID_];
       }
+
     private:
       int textureID_;
       int border_;
+      int x_, y_, w_, h_;
+  };
 
+  class Text {
+    public:
+      Text (const char* path, int size, int x, int y, std::string text = "") {
+        bool fontLoaded = false;
+        for (unsigned int i = 0; i < fontPaths.size(); i++) {
+          if (fontPaths[i] == path) {
+            fontLoaded = true;
+            fontID_ = i;
+            break;
+          }
+        }
+        if (!fontLoaded) {
+          fontID_ = fontPaths.size();
+          fontPaths.push_back(path);
+          fonts.push_back(TTF_OpenFont(path, size));
+        }
+        x_ = x;
+        y_ = y;
+        setText(text);
+      }
+
+      void setText (std::string text) {
+        text_ = text;
+        SDL_Surface* tempSurface = TTF_RenderText_Blended(fonts[fontID_], text_.c_str(), {24, 20, 37, 255});
+        texture_ = SDL_CreateTextureFromSurface(renderer , tempSurface);
+        SDL_FreeSurface(tempSurface);
+      }
+
+      void render () {
+        SDL_Rect destination;
+        int imgW;
+        int imgH;
+        SDL_QueryTexture(texture_, NULL, NULL, &imgW, &imgH);
+        
+        destination.x = x_;
+        destination.y = y_;
+        destination.w = imgW;
+        destination.h = imgH;
+        SDL_RenderCopy(renderer, texture_, NULL, &destination);
+      }
+      
+      void setXY (int x, int y) {
+        x_ = x;
+        y_ = y;
+      }
+
+      ~Text () {
+        SDL_DestroyTexture(texture_);
+      }
+
+    private:
+      std::string text_;
+      SDL_Texture* texture_;
+      int fontID_;
+      int x_, y_;
+  };
+
+  class Image {
+    public:
+      Image(const char* path, int x, int y) {
+        bool textureLoaded = false;
+        for (unsigned int i = 0; i < texturePaths.size(); i++) {
+          if (texturePaths[i] == path) {
+            textureLoaded = true;
+            textureID_ = i;
+            break;
+          }
+        }
+        if (!textureLoaded) {
+          textureID_ = texturePaths.size();
+          texturePaths.push_back(path);
+          SDL_Surface* tempSurface = IMG_Load(path);
+          textures.push_back(SDL_CreateTextureFromSurface(renderer , tempSurface));
+          SDL_FreeSurface(tempSurface);
+        }
+        x_ = x;
+        y_ = y;
+      }
+
+      void render () {
+        SDL_Rect destination;
+        int imgW;
+        int imgH;
+        SDL_QueryTexture(textures[textureID_], NULL, NULL, &imgW, &imgH);
+        
+        destination.x = x_;
+        destination.y = y_;
+        destination.w = imgW;
+        destination.h = imgH;
+        SDL_RenderCopy(renderer, textures[textureID_], NULL, &destination);
+      }
+
+      void setXY (int x, int y) {
+        x_ = x;
+        y_ = y;
+      } 
+
+    private:
+      int x_, y_;
+      int textureID_;
+  };
+
+  class TextBox {
+    public:  
+      TextBox (int x, int y, int w, int h, std::string text = "") :
+      padding_("img/sharp_padding.png", 6, x, y, w, h),
+      text_("fonts/consola.ttf", h, x + 6, y + 2, text) {
+        x_ = x;
+        y_ = y;
+        w_ = w;
+        h_ = h;
+      }
+
+      void setText (std::string text) {
+        text_.setText(text);
+      }
+
+      void render () {
+        padding_.render();
+        text_.render();
+      }
+
+      void setXY (int x, int y) {
+        x_ = x;
+        y_ = y;
+        padding_.setXY(x, y);
+        text_.setXY(x + 6, y + 2);
+      }
+
+    private:
+      Padding padding_;
+      Text text_;
+      int x_, y_, w_, h_;
+  };
+
+  class CycleStep {
+    public:
+      CycleStep (int x, int y) :
+      padding_("img/default_padding.png", 6, x, y, 100, 47),
+      temperatureImage_("img/cycle.png", x + 5, y + 5),
+      durationImage_("img/clock.png", x + 5, y + 26),
+      temperature_(x + 26, y + 5, 69, 16, "0"),
+      duration_(x + 26, y + 26, 69, 16, "0") {
+        x_ = x;
+        y_ = y;
+      }
+
+      void render () {
+        padding_.render();
+        temperatureImage_.render();
+        durationImage_.render();
+        temperature_.render();
+        duration_.render();
+      }
+
+      void setXY (int x, int y) {
+        padding_.setXY(x, y);
+        temperatureImage_.setXY(x + 5, y + 5);
+        durationImage_.setXY(x + 5, y + 26);
+        temperature_.setXY(x + 26, y + 5);
+        duration_.setXY(x + 26, y + 26);
+        x_ = x;
+        y_ = y;
+      }
+
+    private:
+      int x_, y_;
+      Padding padding_;
+      Image temperatureImage_;
+      Image durationImage_;
+      TextBox temperature_;
+      TextBox duration_;
   };
 }
